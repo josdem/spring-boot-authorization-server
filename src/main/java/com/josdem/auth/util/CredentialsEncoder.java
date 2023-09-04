9623b8a0b821a470cfcf773b6050ca4fd6dd26a3
josdem <joseluis.delacruz@gmail.com>
2023-09-04 09:26:16 -0400

    #2 Adding encode64 implementation

--- src/main/java/com/josdem/auth/util/CredentialsEncoder.java
+++ src/main/java/com/josdem/auth/util/CredentialsEncoder.java
@@ -1,8 +1,11 @@
 package com.josdem.auth.util;
 
+import java.util.Base64;
+
 public class CredentialsEncoder {
 
   public static String encode(String username, String password) {
-    return username + ":" + password;
+    String credentialsFormatted = String.format("%s:%s", username, password);
+    return Base64.getUrlEncoder().encodeToString(credentialsFormatted.getBytes());
   }
 }
