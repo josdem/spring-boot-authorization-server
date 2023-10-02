4bc3a0674c5916478ab96f7c9a9249053807c3ef
josdem <joseluis.delacruz@gmail.com>
2023-10-02 18:41:54 -0400

    #6 Updating version 0.3.1

--- src/main/java/com/josdem/auth/config/SecurityConfig.java
+++ src/main/java/com/josdem/auth/config/SecurityConfig.java
@@ -1,32 +1,32 @@
 package com.josdem.auth.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.util.UUID;
-
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
 
   private static final int KEY_SIZE = 2048;
@@ -40,35 +40,34 @@ public class SecurityConfig {
 
   @Bean
   @Order(Ordered.HIGHEST_PRECEDENCE)
-  public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
-      throws Exception {
+  public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
     OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-    return http.formLogin(Customizer.withDefaults()).build();
+    return http.formLogin(withDefaults()).build();
   }
 
   @Bean
   public RegisteredClientRepository registeredClientRepository() {
     RegisteredClient registeredClient =
-        RegisteredClient.withId(UUID.randomUUID().toString())
-            .clientId(applicationConfig.getOidcClientId())
-            .clientSecret(applicationConfig.getOidcClientSecret())
-            .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-            .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-            .redirectUri(applicationConfig.getLoginClientUrl())
-            .redirectUri(applicationConfig.getAuthorizedUrl())
-            .scope(OidcScopes.OPENID)
-            .scope("categories.read")
-            .build();
+            RegisteredClient.withId(UUID.randomUUID().toString())
+                    .clientId(applicationConfig.getOidcClientId())
+                    .clientSecret(applicationConfig.getOidcClientSecret())
+                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                    .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                    .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                    .redirectUri(applicationConfig.getLoginClientUrl())
+                    .redirectUri(applicationConfig.getAuthorizedUrl())
+                    .scope(OidcScopes.OPENID)
+                    .scope("categories.read")
+                    .build();
     RegisteredClient loginClient =
-        RegisteredClient.withId(UUID.randomUUID().toString())
-            .clientId(applicationConfig.getClientId())
-            .clientSecret(applicationConfig.getClientSecret())
-            .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-            .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-            .scope("read")
-            .scope("write")
-            .build();
+            RegisteredClient.withId(UUID.randomUUID().toString())
+                    .clientId(applicationConfig.getClientId())
+                    .clientSecret(applicationConfig.getClientSecret())
+                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                    .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                    .scope("read")
+                    .scope("write")
+                    .build();
     return new InMemoryRegisteredClientRepository(loginClient, registeredClient);
   }
 
@@ -84,9 +83,9 @@ public class SecurityConfig {
     RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
     RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
     return new RSAKey.Builder(publicKey)
-        .privateKey(privateKey)
-        .keyID(UUID.randomUUID().toString())
-        .build();
+            .privateKey(privateKey)
+            .keyID(UUID.randomUUID().toString())
+            .build();
   }
 
   private static KeyPair generateRsaKey() {
@@ -102,7 +101,7 @@ public class SecurityConfig {
   }
 
   @Bean
-  public AuthorizationServerSettings providerSettings() {
-    return AuthorizationServerSettings.builder().issuer(applicationConfig.getServerUrl()).build();
+  public ProviderSettings providerSettings() {
+    return ProviderSettings.builder().issuer(applicationConfig.getServerUrl()).build();
   }
 }
