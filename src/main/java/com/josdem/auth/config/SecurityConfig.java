166186970d91cd097ffbc501d039981403bc5860
josdem <joseluis.delacruz@gmail.com>
2023-10-02 15:21:34 -0400

    #6 Updating provider settings and security filter chain

--- src/main/java/com/josdem/auth/config/SecurityConfig.java
+++ src/main/java/com/josdem/auth/config/SecurityConfig.java
@@ -1,32 +1,32 @@
 package com.josdem.auth.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.util.UUID;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration(proxyBeanMethods = false)
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+@Configuration
 public class SecurityConfig {
 
   private static final int KEY_SIZE = 2048;
@@ -40,9 +40,10 @@ public class SecurityConfig {
 
   @Bean
   @Order(Ordered.HIGHEST_PRECEDENCE)
-  public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
+      throws Exception {
     OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-    return http.formLogin(withDefaults()).build();
+    return http.formLogin(Customizer.withDefaults()).build();
   }
 
   @Bean
@@ -101,7 +102,7 @@ public class SecurityConfig {
   }
 
   @Bean
-  public ProviderSettings providerSettings() {
-    return ProviderSettings.builder().issuer(applicationConfig.getServerUrl()).build();
+  public AuthorizationServerSettings providerSettings() {
+    return AuthorizationServerSettings.builder().issuer(applicationConfig.getServerUrl()).build();
   }
 }
