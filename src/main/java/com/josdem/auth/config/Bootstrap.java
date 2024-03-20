14d9d1807e7712b7431d5d94f64672ed230a5445
josdem <joseluis.delacruz@gmail.com>
2024-03-20 14:43:30 -0400

    #9 Registering default user

--- src/main/java/com/josdem/auth/config/Bootstrap.java
+++ src/main/java/com/josdem/auth/config/Bootstrap.java
@@ -1,11 +1,13 @@
 package com.josdem.auth.config;
 
+import com.josdem.auth.model.Role;
+import com.josdem.auth.model.User;
 import com.josdem.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -13,16 +15,26 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class Bootstrap implements ApplicationListener<ApplicationReadyEvent> {
 
-  private final Environment environment;
   private final UserRepository userRepository;
 
   @Override
   public void onApplicationEvent(final ApplicationReadyEvent event) {
-    log.info("Loading development environment");
-    createDefaultUsers();
+    log.info("Validating default user");
+    createUserWithRole("josdem", "12345678", "contact@josdem.io", Role.USER);
   }
 
-  private void createDefaultUsers() {
-    log.info("Creating default users");
+  void createUserWithRole(String username, String password, String email, Role authority) {
+    if (userRepository.findByUsername(username).isEmpty()) {
+      log.info("Creating user: {}", username);
+      User user = new User();
+      user.setUsername(username);
+      user.setPassword(new BCryptPasswordEncoder().encode(password));
+      user.setEmail(email);
+      user.setRole(authority);
+      user.setFirstName(username);
+      user.setLastName(username);
+      user.setEnabled(true);
+      userRepository.save(user);
+    }
   }
 }
