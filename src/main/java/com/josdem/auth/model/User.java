58acb83fbc69021068b8068407a6811bfddafebb
josdem <joseluis.delacruz@gmail.com>
2024-03-20 11:46:25 -0400

    #9 Adding user values

--- src/main/java/com/josdem/auth/model/User.java
+++ src/main/java/com/josdem/auth/model/User.java
@@ -1,8 +1,23 @@
 package com.josdem.auth.model;
 
+import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class User {}
+public class User {
+  private Long id;
+  private String username;
+  private String password;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String mobile;
+  private Role role;
+  private Boolean enabled = true;
+  private Boolean accountNonExpired = true;
+  private Boolean credentialsNonExpired = true;
+  private Boolean accountNonLocked = true;
+  private LocalDateTime dateCreated = LocalDateTime.now();
+}
